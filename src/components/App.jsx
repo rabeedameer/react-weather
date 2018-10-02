67d6e82338382a90f88e9f68baafbbc5a08e23f6
rabee <rabee.dameer@gmail.com>
2018-10-02 19:58:07 +0200

    deleted unwanted files and created the first component

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <p>this is a test text</p>
+      </div>
+    );
+  }
+}
+
+export default App;
