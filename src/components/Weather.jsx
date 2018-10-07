4e7c6918685673f7a2bed94eedda79b1290fafc9
rabee <rabee.dameer@gmail.com>
2018-10-07 15:53:13 +0200

    set conditionals to display data just when we click getWeather button

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -5,11 +5,10 @@ class Weather extends Component{
   render(){
     return(
       <div>
-        <p>Location: {this.props.city}, {this.props.country}</p>
-        <p>Temperature: {this.props.temperature}</p>
-        <p>Humidity: {this.props.humidity}</p>
-        <p>Conditions: {this.props.condition}</p>
-        <p>{this.props.error}</p>
+        {this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p>}
+        {this.props.temperature && <p>Temperature: {this.props.temperature}°</p>}
+        {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
+        {this.props.condition && <p>Conditions: {this.props.condition}</p>}
       </div>
     );
   }
