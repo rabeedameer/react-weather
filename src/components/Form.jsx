e3381de855848a0b6981f826de3683d6e5642b9b
rabee <rabee.dameer@gmail.com>
2018-10-07 14:06:14 +0200

    comment

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,7 +4,7 @@ import React, {Component} from 'react';
 class  Form extends Component{
   render(){
     return(
-      <form>
+      <form  onSubmit={this.props.getWeather}>{/*it calls the method set in props*/}
         <input type="text" name="city" placeholder="city..."/>
         <input type="text" name="" placeholder="country..."/>
         <button>Show Weather</button>
