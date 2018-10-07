5888ccfd441b799e29b7ff725e113c237d0c292e
rabee <rabee.dameer@gmail.com>
2018-10-07 16:43:08 +0200

    turned the component into a stateless one

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,11 @@
-import React, {Component} from 'react';
+import React from 'react';
 
 
-class  Form extends Component{
-  render(){
-    return(
-      <form  onSubmit={this.props.getWeather}>{/*it calls the method set in props*/}
-        <input type="text" name="city" placeholder="city..."/>
-        <input type="text" name="" placeholder="country..."/>
-        <button>Show Weather</button>
-      </form>
-    );
-  }
-}
+const Form = (props)=>(
+  <form  onSubmit={props.getWeather}>{/*it calls the method set in props*/}
+  <input type="text" name="city" placeholder="city..."/>
+  <input type="text" name="" placeholder="country..."/>
+  <button>Show Weather</button>
+</form>);
 
 export default Form;
