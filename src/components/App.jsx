4c0132c2859336cbff92ba08cfa16436d558a269
rabee <rabee.dameer@gmail.com>
2018-10-07 14:11:51 +0200

    preventDefault to the method

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,8 @@ const API_KEY = 'c5b27ea3cfd3a70d0a3d7ce16693bebc';
 const baseURL= 'http://api.openweathermap.org/data/2.5/';
 
 class App extends Component{
-    getWeather = async()=>{// here we defined he method to get a call for the API
+    getWeather = async(e)=>{// here we defined he method to get a call for the API
+      e.preventDefault();
       const callApi = await fetch(`${baseURL}weather?q=Hama&appid=${API_KEY}&units=metric`);
       const data = await callApi.json();
       console.log(data);
