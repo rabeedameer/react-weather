865b9f75149bbbfe6a8f77cea5f1051c26034276
rabee <rabee.dameer@gmail.com>
2018-10-07 14:41:43 +0200

    initialized the state to an Obj and set initial keys

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,12 +9,32 @@ const API_KEY = 'c5b27ea3cfd3a70d0a3d7ce16693bebc';
 const baseURL= 'http://api.openweathermap.org/data/2.5/';
 
 class App extends Component{
+  state = {
+    temperature:'',
+    city: '',
+    country:'',
+    humidity:'',
+    minTemp: '',
+    maxTemp:'',
+    condition: '',
+    error:''
+  }
     getWeather = async(e)=>{// here we defined he method to get a call for the API
       e.preventDefault();
       const city = e.target.elements.city.value;
       const callApi = await fetch(`${baseURL}weather?q=${city}&appid=${API_KEY}&units=metric`);
       const data = await callApi.json();
       console.log(data);
+      this.setState({
+        temperature :data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        minTemp: data.main.temp_min,
+        maxTemp: data.main.temp_max,
+        condition: data.weather[0].description,
+        error:''
+      })
     }
   render(){
     return(
