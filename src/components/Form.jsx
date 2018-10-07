b099eaffbe8eeac12651e72f00f2c53d32bb6832
rabee <rabee.dameer@gmail.com>
2018-10-07 13:56:31 +0200

    added the simple form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,9 +4,11 @@ import React, {Component} from 'react';
 class  Form extends Component{
   render(){
     return(
-      <div>
-        This is form component
-      </div>
+      <form>
+        <input type="text" name="city" placeholder="city..."/>
+        <input type="text" name="" placeholder="country..."/>
+        <button>Show Weather</button>
+      </form>
     );
   }
 }
