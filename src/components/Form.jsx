71445a45ad04604248be813106cd11fca17d4ee2
rabee <rabee.dameer@gmail.com>
2018-10-26 18:59:44 +0200

    installed redux

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const Form = (props)=>(
   <form  onSubmit={props.getWeather}>{/*it calls the method set in props*/}
-  <input type="text" name="city" placeholder="city..." required/>
-  <input type="text" name="country" placeholder="country..."/>
-  <button>Show Weather</button>
+  <input type="text" name="city" placeholder="City..." required/>
+  <input type="text" name="country" placeholder="Country..."/>
+  <button className="btn btn-outline-success">Check Weather</button>
 </form>);
 
 export default Form;
